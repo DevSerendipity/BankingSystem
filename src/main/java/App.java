4de1a4c42cb28e5ba1720a9b32ec9d50dc8cc5e8
Islam <islamsakrak2002@gmail.com>
2022-02-17 15:58:42 +0100

    Making it easier to add a customer

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,15 +5,19 @@ public class App {
     private static final BankingSystem bankingSystem = new BankingSystem();
 
     public static void main(String[] args) {
-        bankingSystem.getCustomers().add(new Customer("Man", 100));
-        bankingSystem.getCustomers().add(new Customer("Tom", 100));
+        addCustomer("Man");
+        addCustomer("Tom");
         bankingSystem.getBankDetails();
         getAllCustomers();
     }
 
+    private static void addCustomer(String customer) {
+        bankingSystem.add(new Customer(customer, 100));
+    }
+
     private static void getAllCustomers() {
-        for (Customer value : bankingSystem.getCustomers()) {
-            System.out.println("Name : " + value.getName() + "\nBalance " + value.getBalance() + "$\nAccountID " + new BigInteger(53, new Random()));
+        for (Customer customer : bankingSystem.getCustomers()) {
+            System.out.println("Name : " + customer.getName() + "\nBalance " + customer.getBalance() + "$\nAccountID " + new BigInteger(53, new Random()));
         }
     }
 }
