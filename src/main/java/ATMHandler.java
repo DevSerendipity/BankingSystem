1cbf279f7df2347ac74d6872cd3b18e7f6f5af7e
FoolProof <islamsakrak2002@gmail.com>
2022-09-12 17:32:22 +0200

    Making the instance within the method

--- src/main/java/ATMHandler.java
+++ src/main/java/ATMHandler.java
@@ -4,10 +4,10 @@ import customer.CustomerPrinter;
 import java.util.Scanner;
 
 public class ATMHandler {
-    private final BalanceHandler balanceHandler = new BalanceHandler();
 
     void atmActions(Customer customer) {
         CustomerPrinter customerPrinter = new CustomerPrinter();
+        BalanceHandler balanceHandler = new BalanceHandler();
         System.out.println("Hello " + customer.getName() + customerPrinter.printCustomerMessage());
         Scanner input = new Scanner(System.in);
         int atm = input.nextInt();
